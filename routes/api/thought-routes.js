8394a015577f6737e123a5b54015cac5f48b4ecb
Kelerik <vincent.ng39@gmail.com>
2022-02-21 14:36:59 -0500

    thought routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const {
+   addThought,
+   removeThought,
+   addReaction,
+   removeReaction,
+} = require("../../controllers/thought-controller");
+
+router.route("/:userId").post(addThought);
+
+router.route("/:userId/:thoughtId").delete(removeThought);
+
+router.route("/:userId/:thoughtId/reactions").post(addReaction);
+router
+   .route("/:userId/:thoughtId/reactions/:reactionId")
+   .delete(removeReaction);
+
+module.exports = router;
